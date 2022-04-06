acf6f61a6905c83509559bb796cd3b3340bcf229
Marcos Uchoa <marcospacheco10111@gmail.com>
2022-04-06 12:35:56 -0300

    Curso ASP NET MVC Core finalizado.
          - Razor
          - Entity Framework Core
          - Migrations
          - Models
          - ViewModels
          - Parameters bind

---
+++ MvcFilme/Models/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcFilme.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace MvcFilme.Models
+{
+    public class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                using (var context = new MvcFilmeContext(
+                    scope.ServiceProvider.GetRequiredService<DbContextOptions<MvcFilmeContext>>()))
+                {
+                    if ( await context.Filme.AnyAsync())
+                        return;
+                    context.Filme.AddRange(
+                        new Filme
+                        {
+                            Titulo = "A Bela e a Fera",
+                            Lancamento = DateTime.Parse("2017-3-16"),
+                            Genero = "Fantasia/Romance",
+                            Preco = 7.99M,
+                            Classificacao = "L"
+                        },
+                        new Filme
+                        {
+                            Titulo = "Django Livre",
+                            Lancamento = DateTime.Parse("2012-1-18"),
+                            Genero = "Faroeste",
+                            Preco = 12.99M,
+                            Classificacao = "16"
+                        }
+                        );
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
